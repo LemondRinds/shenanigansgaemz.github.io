05d31de410d2d46a179ba3f5c6be8a691131edc1
LemondRinds <diveaway89@gmail.com>
2023-11-28 23:01:58 -0500

    Update onload.js

--- onload.js
+++ onload.js
@@ -1,4 +1,20 @@
 window.onload = (event) => {
+  
   console.log("page is fully loaded");
   
+  $.get("navigation.html", function(data){
+				$("#nav_placeholder").replaceWith(data);
+		});
+
+  /// provide some labels and comments
+  $(function(){
+    var current = location.pathname;
+    $('li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+  })
 };
